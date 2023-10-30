9ee6c75f7f9baa44a06ff5de5a38e02f190fc9a1
Jacinta100 <ugwujacintafavour@gmail.com>
2023-10-30 01:28:57 +0100

    Second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// let signupButton = document. querySelector('#Signup_btn');
+// Signup_btn. addEventListner ('click', function (){
+//   console.log('Place your order');
+// })
+let signupButton = document.querySelector('#Signup_btn'); // Use the correct variable name
+signupButton.addEventListener('click', function () {
+  console.log('Place your order');
+});
