157d5c456022f3e21bbead3f522073b5e21dd236
Jacinta100 <ugwujacintafavour@gmail.com>
2023-11-04 21:53:11 +0100

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-// let signupButton = document. querySelector('#Signup_btn');
-// Signup_btn. addEventListner ('click', function (){
-//   console.log('Place your order');
-// })
-let signupButton = document.querySelector('#Signup_btn'); // Use the correct variable name
-signupButton.addEventListener('click', function () {
-  console.log('Place your order');
+const form = document.getElementById('signup-form');
+const submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener('click', function () {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // You can perform form validation here
+    if (name === '' || email === '' || password === '') {
+        alert('Please fill in all fields.');
+    } else {
+        // You can perform form submission or other actions here
+        alert('Form submitted successfully!');
+        form.reset(); // Reset the form after submission
+    }
 });
